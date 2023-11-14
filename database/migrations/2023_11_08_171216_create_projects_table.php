680effa2472f1e995615cb73a18586b341507b78
“LiciaLicari” <licianbsplicari@gmail.com>
2023-11-14 14:39:26 +0100

    Initial Commit

---
+++ database/migrations/2023_11_08_171216_create_projects_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('cover_image');
+            $table->string('title');
+            $table->string('slug'); //takes the title and puts it in the URL
+            $table->text('description')->nullable();
+            $table->string('github', 2048)->nullable();
+            $table->string('link', 2048)->nullable();
+            $table->string('type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('projects');
+    }
+};
